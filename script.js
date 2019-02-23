21f67bd789f8fd71304206fabcfb2283d12c6bd7
Jens Erhardt <j.erhardt@lum-gmbh.de>
2019-02-23 12:51:18 +0100

    Fixed x axis. Added title.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const api = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01"
+const api = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-31&end=2019-02-22"
 
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function() {
   fetch(api)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      const data = convertData(response);
+      const data = parse(response);
 
       drawLineChart(data);
     })
+    .catch((err) => { console.log(err) })
 });
 
-function convertData(data) {
+function parse(data) {
   let arr = [];
 
   for (const i in data.bpi) {
@@ -26,27 +27,40 @@ function convertData(data) {
 
 function drawLineChart(data) {
 
-  console.log(data)
-
-  var svgWidth = 600, svgHeight = 400;
-  var margin = {
+  const svgWidth = 600, svgHeight = 400;
+  const margin = {
     top: 20, right: 20,
     bottom: 30, left: 50
   };
 
-  var width = svgWidth - margin.left - margin.right;
-  var height = svgHeight - margin.top - margin.bottom;
+  const width = svgWidth - margin.left - margin.right;
+  const height = svgHeight - margin.top - margin.bottom;
 
-  var svg = d3.select('svg')
+  let svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-  var g = svg.append("g")
+  const g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var x = d3.scaleTime().rangeRound([0, width]);
+    svg.append("text")
+    .attr("x", (svgWidth / 2))             
+    .attr("y", (margin.top / 2 + 10))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "16px") 
+    .style("font-family", "helvetica")
+    .style("text-decoration", "underline")  
+    .text("Bitcoin Price Index 2019-01-31 to 2019-02-22");
+  
+  const x = d3.scaleTime().rangeRound([0, width]);
 
-  var y = d3.scaleLinear().rangeRound([height, 0]);
+  const y = d3.scaleLinear().rangeRound([height, 0]);
+
+  let line = d3.line()
+    .x(function (d) { return x(d.date) })
+    .y(function (d) { return y(d.value) })
+  x.domain(d3.extent(data, function (d) { return d.date }));
+  y.domain(d3.extent(data, function (d) { return d.value }));
 
   g.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -63,12 +77,6 @@ function drawLineChart(data) {
     .attr("dy", "0.71em")
     .attr("text-anchor", "end")
     .text("Price ($)");
-
-  var line = d3.line()
-    .x(function (d) { return x(d.date) })
-    .y(function (d) { return y(d.value) })
-  x.domain(d3.extent(data, function (d) { return d.date }));
-  y.domain(d3.extent(data, function (d) { return d.value }));
 
   g.append("path")
     .datum(data)
