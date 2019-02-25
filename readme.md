7c0b77e0b62c978f9b472d098d40efc839236d9a
Jens Erhardt <j.erhardt@lum-gmbh.de>
2019-02-25 18:08:00 +0100

    Added simple linear regression based on fetched data.

--- readme.md
+++ readme.md
@@ -1,10 +1,14 @@
 
 <h1>Chart Works with d3</h1>
 
-<p>This is just some practice using HTML, CSS, Bootstrap, Javascript, JQuery and the d3 library to visualize different data.</p>
+<p>This was actually a practice drawing a line chart with d3 for work. But then it turned out to be so much fun that I decided to add more features such as regression and interactivity. For further details please see the list below</p>
+
+<p>Technoligies used: HTML, CSS, Bootstrap, Javascript, JQuery and d3 library to visualize different data.</p>
 
 <p>1.) Fetching data from api.coindesk.com and showing it in a line chart. Done following this <a href="https://medium.freecodecamp.org/learn-to-create-a-line-chart-using-d3-js-4f43f1ee716b">Tutorial</a>.</p>
 
-<p>2.) Copy of 1.) generating and visulizating random data using a maximum number from an input.</p>
+<p>2.) Simple linear regression based on fetched data.</p>
 
-<a href="https://jenserhardt.github.io/chart-works-with-d3/. ">See the working example here</a>
+<p>3.) Copy of 1.) generating and visulizating random data using a maximum number from an input.</p>
+
+<a href="https://jenserhardt.github.io/chart-works-with-d3/. ">Click here to see working example</a>
--- script.js
+++ script.js
@@ -10,14 +10,27 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then(function (response) {
       const data = parse(response);
-
       drawLineChart(data);
-      
-      calculateRandom(data)
+
+      const linear = simpleLinearRegression(response)
+      const prognoses = parseLinear(linear, data)
+      drawLineChart2(prognoses)
+
+      calculateRandomByMax(data)
       drawLineChart1(data);
     })
     .catch((err) => { console.log(err) })
 });
+
+function arrayOf(data) {
+  let arr = [];
+
+  for (const i in data.bpi) {
+    arr.push(data.bpi[i])
+
+  }
+  return arr;
+}
 
 function parse(data) {
   let arr = [];
@@ -28,7 +41,76 @@ function parse(data) {
       value: +data.bpi[i]
     });
   }
+
   return arr;
+}
+
+function parseLinear(linear, data) {
+  let arr = [];
+  
+  for (const i in linear) {
+    console.log(data[i].date)
+    arr.push({
+      date: i,
+      value: +linear[i]
+    });
+  }
+  console.log("parseLinear", arr)
+  return arr;
+}
+
+function calculateRandomByMax(data) {
+  const max = $("#maximum").val()
+  const arr = [];
+
+  for (let date of data) {
+    date.value = Math.floor((Math.random() * max) + 1)
+  }
+
+  return arr;
+}
+
+function simpleLinearRegression(data) {
+  const arr = arrayOf(data)
+  const linear = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    linear.push(linearProject(arr, i))
+  }
+  return linear;
+}
+
+function LineFitter() {
+  this.count = 0;
+  this.sumX = 0;
+  this.sumX2 = 0;
+  this.sumXY = 0;
+  this.sumY = 0;
+}
+
+LineFitter.prototype = {
+  'add': function (x, y) {
+    this.count++;
+    this.sumX += x;
+    this.sumX2 += x * x;
+    this.sumXY += x * y;
+    this.sumY += y;
+  },
+  'project': function (x) {
+    var det = this.count * this.sumX2 - this.sumX * this.sumX;
+    var offset = (this.sumX2 * this.sumY - this.sumX * this.sumXY) / det;
+    var scale = (this.count * this.sumXY - this.sumX * this.sumY) / det;
+    return offset + x * scale;
+  }
+};
+
+function linearProject(data, x) {
+  let fitter = new LineFitter();
+
+  for (let i = 0; i < data.length; i++) {
+    fitter.add(i, data[i]);
+  }
+  return fitter.project(x);
 }
 
 function drawLineChart(data) {
@@ -94,14 +176,6 @@ function drawLineChart(data) {
     .attr("d", line);
 }
 
-function calculateRandom(data) {
-  const max = $("#maximum").val()
-
-  for (let date of data) {
-    date.value = Math.floor((Math.random() * max) + 1)
-  }
-}
-
 function drawLineChart1(data) {
 
   const svgWidth = 600, svgHeight = 400;
@@ -165,4 +239,65 @@ function drawLineChart1(data) {
     .attr("d", line);
 }
 
+function drawLineChart2(data) {
 
+  const svgWidth = 600, svgHeight = 400;
+  const margin = {
+    top: 30, right: 20,
+    bottom: 30, left: 50
+  };
+
+  const width = svgWidth - margin.left - margin.right;
+  const height = svgHeight - margin.top - margin.bottom;
+
+  let svg = d3.select(".line-chart-2")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+  const g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("text")
+    .attr("x", (svgWidth / 2))
+    .attr("y", (margin.top / 2 + 10))
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("font-family", "helvetica")
+    .style("text-decoration", "underline")
+    .text("Linear Regression for BPI based on " + from + " to " + to);
+
+  const x = d3.scaleLinear().rangeRound([0, width]);
+
+  const y = d3.scaleLinear().rangeRound([height, 0]);
+
+  let line = d3.line()
+    .x(function (d) { return x(d.date) })
+    .y(function (d) { return y(d.value) })
+  x.domain(d3.extent(data, function (d) { return d.date }));
+  y.domain(d3.extent(data, function (d) { return d.value }));
+
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove();
+
+  g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Price ($)");
+
+  g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+}
